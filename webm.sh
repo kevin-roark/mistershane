39c79f4d721c175f7def1ae118927ef22d6bb1e1
Kevin Roark <ker2143@columbia.edu>
2015-05-19 00:27:25 -0400

    lower quality webm :)

--- webm.sh
+++ webm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 in=$1
 out=$2
-ffmpeg -threads 16 -i $in -vcodec libvpx -s 320x240 -aspect 4:3 -acodec libvorbis $out
+ffmpeg -threads 16 -i $in -vcodec libvpx -acodec libvorbis -b:v 0.1M $out
